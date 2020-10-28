8db5016d1bb87adf6b9e41786cfe87416b991de4
Andrea Valenzano <andrea@localhost.localdomain>
2020-10-28 04:22:07 +0100

    first commit

---
+++ Makefile
@@ -0,0 +1,23 @@
+
+EXEC = cat
+CC = gcc
+CFLAGS = -Wall -pedantic -Werror
+DEBUGFLAGS = -g -fsanitize=address -fsanitize=undefined
+TESTFILES = test0 test1 test2 test3 test4
+all: $(EXEC)
+
+cat:  cat.c
+	$(CC) $(CFLAGS) $(DEBUGFLAGS) -o $@ $^
+
+test_files:
+	touch $(TESTFILES)
+
+clean:
+	rm -f $(EXEC) $(TESTFILES)
+
+
+.PHONY: test_files clean all
+
+
+
+
---
+++ cat.c
@@ -0,0 +1,104 @@
+/*
+ * cat
+ *
+ * Versione: 1.0
+ * Autore: Andre Valenzano
+ * Data: 28-10-2020
+ *
+ * Programma per concatenare FILE(s) sullo standard output
+ * In caso di assenza di FILE legge dallo standard input
+ *
+ * Programma creato durante il corso di Sistemi di Elaborazione e
+ * Trasmissione dell'Informazione presso l'Università degli Studi di Genova,
+ * anno accademico 2020/2021
+ *
+ */
+
+/**** Includo le librerie ****/
+#include <stdio.h> /* perror */
+#include <stdlib.h> /* exit */
+#include <string.h> /* strcmp */
+#include <unistd.h> /* read, write*/
+#include <sys/types.h> /*       */
+#include <sys/stat.h> /* open  */
+#include <fcntl.h>   /*       */
+#include <errno.h>
+
+/**** Definisco le variabili globali ****/
+#define STDIN 0 /* STDIN File Descriptor */
+#define STDOUT 1 /* STDOUT File Descriptor */
+#define STDERR 2 /* STDERR File Descriptor */
+
+#define STDIN_BUFF_SIZE 1024 /* 1 KiB */
+#define FILE_BUFF_SIZE 1048576 /* 1 MiB */
+
+int main(int argc, char** argv){
+
+/**** Nel caso in cui non specifico un FILE negli argomenti, leggo da STDIN ****/
+    if(argc == 1){
+
+        char BUFF[STDIN_BUFF_SIZE];
+        ssize_t read_bytes;
+
+        for(;;){
+            if((read_bytes = read(STDIN, BUFF, STDIN_BUFF_SIZE)) == -1){
+                /* Se non riesco a leggere dallo standard input, esco dal programma riportanndo il messaggio di errore*/
+                perror("Can't read thrue STDIN");
+                exit(EXIT_FAILURE);
+            }
+            if((write(STDOUT, BUFF, read_bytes)) == -1){
+                /* Se non riesco a scrivere sullo standard output, esco dal programma riportando il messaggio di errore*/
+                perror("Can't write to STDOUT");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+/**** Altrimenti leggo da FILE, nel caso di più parametri concateno il contenuto dei vari FILEs ****/
+    else{
+        /**** Firma dell'autore ****/
+        if((strcmp(argv[1],"mother_of_all_questions")) == 0){
+            goto mother_of_all_questions;
+        }
+
+        /**** Apro i vari FILEs dati come argomento e li concateno su standard output****/
+        int file_fd;
+        char BUFF[FILE_BUFF_SIZE];
+        ssize_t read_bytes;
+        size_t total_bytes = 0;
+
+        for(unsigned int i = 1; i < argc; i++){
+            if((file_fd = open(argv[i], O_RDONLY)) == -1){
+                /* Se non riesco ad aprire il FILE, esco dal programma riportanndo il messaggio di errore*/
+                perror(argv[i]);
+                close(file_fd);
+                exit(EXIT_FAILURE);
+            }
+            if((read_bytes = read(file_fd, (BUFF + total_bytes), (FILE_BUFF_SIZE - total_bytes))) == -1){
+                /* Se non riesco a leggere da FILE, esco dal programma riportanndo il messaggio di errore*/
+                perror("Can't read thrue FD");
+                close(file_fd);
+                exit(EXIT_FAILURE);
+            }
+            close(file_fd);
+
+            total_bytes += read_bytes;
+        }
+
+        if((write(STDOUT, BUFF, total_bytes)) == -1){
+            /* Se non riesco a scrivere sullo standard output, esco dal programma riportando il messaggio di errore*/
+            perror("Can't write to STDOUT");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+exit(EXIT_SUCCESS);
+
+mother_of_all_questions:
+    if((write(STDOUT, "\n\tFoto Piedi?\n\n", 15)) == -1){
+        /* Se non riesco a scrivere sullo standard output, esco dal programma riportando il messaggio di errore*/
+        perror("Can't write to STDOUT");
+        exit(EXIT_FAILURE);
+    }
+exit(EXIT_SUCCESS);
+}
