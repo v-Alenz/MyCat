05e24f10c4c4f8bac7c4efbde16481f4c4ac71db
Andrea Valenzano <andrea@localhost.localdomain>
2020-10-28 23:17:04 +0100

    gestione dinamica della memoria

--- mycat.c
+++ mycat.c
@@ -30,7 +30,7 @@
 #define STDERR 2 /* STDERR File Descriptor */
 
 #define STDIN_BUFF_SIZE 1024 /* 1 KiB */
-#define FILE_BUFF_SIZE 1048576 /* 1 MiB */
+#define FILE_BUFF_SIZE 4096 /* 4 MiB */
 
 int main(int argc, char** argv){
 
@@ -63,9 +63,10 @@ int main(int argc, char** argv){
 
         /**** Apro i vari FILEs dati come argomento e li concateno su standard output****/
         int file_fd;
-        char BUFF[FILE_BUFF_SIZE];
+        char* BUFF = malloc(FILE_BUFF_SIZE);
         ssize_t read_bytes;
         size_t total_bytes = 0;
+        size_t buff_size = FILE_BUFF_SIZE;
 
         for(unsigned int i = 1; i < argc; i++){
             if((file_fd = open(argv[i], O_RDONLY)) == -1){
@@ -74,15 +75,22 @@ int main(int argc, char** argv){
                 close(file_fd);
                 exit(EXIT_FAILURE);
             }
-            if((read_bytes = read(file_fd, (BUFF + total_bytes), (FILE_BUFF_SIZE - total_bytes))) == -1){
+read_:
+            if((read_bytes = read(file_fd, (BUFF + total_bytes), (buff_size - total_bytes))) == -1){
                 /* Se non riesco a leggere da FILE, esco dal programma riportanndo il messaggio di errore*/
                 perror("Can't read thrue FD");
                 close(file_fd);
                 exit(EXIT_FAILURE);
             }
-            close(file_fd);
-
             total_bytes += read_bytes;
+
+            if(buff_size == total_bytes){
+                buff_size += FILE_BUFF_SIZE;
+                BUFF = realloc(BUFF, buff_size);
+                goto read_;
+            }
+
+            close(file_fd);
         }
 
         if((write(STDOUT, BUFF, total_bytes)) == -1){
@@ -90,6 +98,8 @@ int main(int argc, char** argv){
             perror("Can't write to STDOUT");
             exit(EXIT_FAILURE);
         }
+
+        free(BUFF);
     }
 
 exit(EXIT_SUCCESS);
@@ -102,3 +112,16 @@ mother_of_all_questions:
     }
 exit(EXIT_SUCCESS);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
